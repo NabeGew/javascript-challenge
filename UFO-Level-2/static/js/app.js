13a7232a074c8f03f1926ef57fadea6828857e91
Gabriel New <gabrielnew@newusers-MacBook-Pro.local>
2020-12-21 18:41:31 -0500

    Push Push

--- UFO-Level-2/static/js/app.js
+++ UFO-Level-2/static/js/app.js
@@ -1,71 +1,52 @@
 // from data.js
 var tableData = data;
 
-//** Level 1: Automatic Table and Date Search
-//*** Write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
 
-// Get a reference to the table body
-var tbody = d3.select("tbody");
-
-// Loop through each ufo object in the data array
-tableData.forEach((ufo) => {
-
-	// Use d3 to append one table row `tr` for each ufo object
-	var row = tbody.append("tr");
-
-	// Use `Object.entries` and forEach to iterate through keys and values
-	Object.entries(ufo).forEach(([key, value]) => {
-
-		// Use d3 to append one cell per ufo object value (date, city, state, country, shape, duration, and comments)  
-		var cell = row.append("td");
-		cell.text(value);
-	});
-});
-
-//*** Use a date form in your HTML document and write JavaScript code that will listen
-//for events and search through the date/time column to find rows that match user input.
-
-// Select the button
+// Creating References to tbody, input and button
+var $tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
+var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
-// Select the form
-var form = d3.select("form");
 
-// Create event handlers 
-button.on("click", runEnter);
-form.on("submit", runEnter);
+// Inputing the data into the HTML
+var addData = (dataInput) => {
+    dataInput.forEach(ufoSightings => {
+        var row = $tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSightings[column])
+        )
+    });
+}
 
-// Complete the event handler function for the form
-function runEnter() {
+addData(tableData);
 
-  // Prevent the page from refreshing
+
+//Create eventListener for button
+button.on("click", () => {
+
   d3.event.preventDefault();
-
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select(".form-control");
-
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
-
-  // Use the form input to filter the data by datetime
-	var results = tableData.filter(ufo => ufo.datetime === inputValue);
-	
-	// Clear out current contents in the table
-	tbody.html("");
-
-	// Handle no matching results
-	if (results.length === 0) {
-		tbody.text(`No ufo sightings on ${inputValue}.`);
-	}
-
-	// Handle matching results
-	else {
-		results.forEach((ufo) => {
-			var row = tbody.append("tr");
-			Object.entries(ufo).forEach(([key, value]) => {
-				var cell = row.append("td");
-				cell.text(value);
-			});
-		});
-	};
-};
+  var inputDate = inputFieldDate.property("value").trim();
+  // console.log(inputDate)
+  var inputCity = inputFieldCity.property("value").toLowerCase().trim();
+  // console.log(inputCity)
+  var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
+  // console.log(filterDate)
+  var filterCity = tableData.filter(tableData => tableData.city === inputCity);
+  // console.log(filterCity)
+  var filterCombinedData = tableData.filter(tableData => tableData.datetime === inputDate && tableData.city === inputCity);
+  // console.log(filterCombinedData)
+  $tbody.html("");
+    let response = {
+        filterDate, filterCity, filterCombinedData
+    }
+    if(response.filterCombinedData.length !== 0) {
+      addData(filterCombinedData);
+  }
+  else if(response.filterCombinedData.length === 0 && ((response.filterDate.length !== 0 || response.filterCity.length !== 0))) {
+    addData(filterDate) || addData(filterCity);
+}
+else {
+  $tbody.append("tr").append("td").text("No Sightings Here...Move On...");
+}
+})
